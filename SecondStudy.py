68a98a71c35c79bbf82731bf5c4fce03be5cf9fa
dimas nurzullah <dimasnurzullah@gmail,com>
2024-12-14 21:27:14 +0700

    First Study Python

---
+++ SecondStudy.py
@@ -0,0 +1,9 @@
+# typeString
+name = "Otong Surendra"
+age = 25
+ipk =3.2
+nilai_ipk = float(ipk)
+if  age:
+    started_age=age-4
+print(f"perkenalkan nama saya:{name},saya masuk kuliah umur {started_age} dengan umur saat ini {age} memperoleh nilai kelulusan {nilai_ipk}")
+
---
+++ firstStudy.py
@@ -0,0 +1,11 @@
+# belajar terkai pengenalan dasar
+name = "Dimas Nurzullah Eka Nugraha"
+age = 26
+email = "dimasnurzullah@gmail.com"
+print (f"hello nama saya {name},saya berumur {age} dan memiliki email {email}")
+# boolean
+for_sale = True
+if for_sale:
+    print(f"sintak forsale jika {for_sale}  berhasil jalan")
+else: 
+    print(f"sintak for_sale jika {for_sale} tidak jalan")
